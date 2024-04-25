eb10ba66feef8ba8def4d0076048ce4af3cc9a03
Adam Newman <c.adamnewman@gmail.com>
2024-04-25 08:56:58 -0700

    Init commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<head>    
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=devide-width, initial-scale=1.0">
+  <title>Ticketing App</title>
+</head>
+<body>
+  <h3>Hi</h3>
+</body>
+</html>
